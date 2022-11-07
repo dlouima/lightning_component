ebd9fdb6786d56efd6eeeb3631c150697a2bd4c9
Louima Dieubon <louimadieubon@Louimas-MacBook-Pro.local>
2022-11-07 12:14:37 -0500

    added the memory game project

--- force-app/main/default/lwc/memoryGame/memoryGame.js
+++ force-app/main/default/lwc/memoryGame/memoryGame.js
@@ -3,7 +3,13 @@ import {loadStyle} from 'lightning/platformResourceLoader'
 import fontawesome from '@salesforce/resourceUrl/fontawesome'
 
 export default class MemoryGame extends LightningElement {
-isLoaded= false
+  isLibLoaded = false
+  openedCards =[]
+  matchedCard=[]
+  totalTime='00:00'
+  moves=0
+  timerRef
+  showCongratulations = false
 
 cards=[
   {id:1, listClass:"card", type:'diamond', icon:'fa fa-diamond'},
@@ -26,24 +32,115 @@ cards=[
 ]
 
 
-  renderedCallback(){
-    if(this.isLoaded){
-      return
-    }else{
-      loadStyle(this, fontawesome+'/fontawesome/css/font-awesome.min.css').then(()=>{
-        console.log('loaded successfully')
-        this.isLoaded=true
-      }).catch( error => {
-        console.error(error)
-      })
-
+get gameRating(){
+  let stars =  this.moves<12 ? [1,2,3]:this.moves>=13 ? [1,2]:[1]
+return this.matchedCard.length ===16 ? stars :[]
+}
+displayCard(event){
+  let currCard = event.target
+  currCard.classList.add("open", "show", "disabled")
+  this.openedCards = this.openedCards.concat(event.target)
+  const len = this.openedCards.length
+  if(len === 2){
+    this.moves = this.moves+1
+    if(this.moves === 1){
+        this.timer()
     }
-
+    if(this.openedCards[0].type === this.openedCards[1].type){
+        this.matchedCard = this.matchedCard.concat(this.openedCards[0], this.openedCards[1])
+        this.matched()
+    } else {
+        this.unmatched()
+    }
   }
+}
 
-  displayCard(event){
-    let currentCard= event.target
-    currentCard.classList.add('open', 'show', 'disable')
-
+matched(){
+  this.openedCards[0].classList.add("match", "disabled")
+  this.openedCards[1].classList.add("match", "disabled")
+  this.openedCards[0].classList.remove("show", "open")
+  this.openedCards[1].classList.remove("show", "open")
+  this.openedCards=[]
+  if(this.matchedCard.length === 16){
+      window.clearInterval(this.timerRef)
+      this.showCongratulations = true
   }
+}
+unmatched(){
+this.openedCards[0].classList.add("unmatched")
+this.openedCards[1].classList.add("unmatched")
+this.action('DISABLE')
+setTimeout(()=>{
+    this.openedCards[0].classList.remove("show", "open", "unmatched")
+    this.openedCards[1].classList.remove("show", "open", "unmatched")
+    this.action('ENABLE')
+    this.openedCards=[]
+},1100)
+}
+
+action(action){
+  let cards = this.template.querySelectorAll('.card')
+  Array.from(cards).forEach(item=>{
+      if(action === 'ENABLE'){
+          let isMatch = item.classList.contains('match')
+          if(!isMatch){
+              item.classList.remove('disabled')
+          }
+      }
+      if(action === 'DISABLE'){
+          item.classList.add('disabled')
+      }
+  })
+}
+
+timer(){
+  let startTime = new Date()
+  this.timerRef = setInterval(()=>{
+    let diff = new Date().getTime() - startTime.getTime()
+    let d = Math.floor(diff/1000)
+    const m = Math.floor(d % 3600 / 60);
+    const s = Math.floor(d % 3600 % 60);
+    const mDisplay = m>0 ? m+(m===1? "minute, ":" minutes, "):""
+    const sDisplay = s>0 ? s+(s===1? "second":" seconds"):""
+    this.totalTime = mDisplay + sDisplay
+  }, 1000)
+}
+
+shuffle(){
+this.showCongratulations = false
+this.openedCards =[]
+this.matchedCard=[]
+this.totalTime='00:00'
+this.moves=0
+window.clearInterval(this.timerRef)
+let elem = this.template.querySelectorAll('.card')
+Array.from(elem).forEach(item=>{
+    item.classList.remove("show", "open", "match", "disabled")
+})
+/***shuffling and swaping logic */
+let array = [...this.cards]
+let counter = array.length
+while(counter>0){
+    let index = Math.floor(Math.random()*counter)
+    counter--
+
+    let temp = array[counter]
+    array[counter] = array[index]
+    array[index] = temp
+}
+this.cards = [...array]
+}
+renderedCallback(){
+if(this.isLibLoaded){
+    return
+} else {
+    loadStyle(this,fontawesome+'/fontawesome/css/font-awesome.min.css').then(()=>{
+        console.log("loaded successfully")
+    }).catch(error=>{
+        console.error(error)
+    })
+    this.isLibLoaded = true
+}
+}
+
 }
